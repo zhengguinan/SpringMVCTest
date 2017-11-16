764bd36a77a0d07c0abd9363aa2f4b5dd5115a37
zhengguinan <zhengguinan@jd.com>
2017-11-16 17:02:43 +0800

    初始化

---
+++ src/main/java/org/zhenggn/springmvctest/controller/HelloController.java
@@ -0,0 +1,35 @@
+package org.zhenggn.springmvctest.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.jws.WebParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * <p>Title: SpringMVCTest</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Company: www.jd.com</p>
+ *
+ * @author zhengguinan
+ * @version 1.0
+ * @date 2017/11/16
+ */
+public class HelloController implements Controller{
+
+    private static final Log logger = LogFactory.getLog(HelloController.class);
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        logger.info("handleRequest 被调用");
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message","Hello World!");
+        mv.setViewName("/WEB-INF/content/welcome.jsp");
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/content/welcome.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhengguinan
+  Date: 2017/11/16
+  Time: 16:46
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${requestScope.message}
+</body>
+</html>
